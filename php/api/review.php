eb7e09b3be68bba9d2a9fd95ade3f63d5d956684
William Cairns <cairnswm@gmail.com>
2025-05-27 15:52:48 +0200

    Fixes

--- php/api/review.php
+++ php/api/review.php
@@ -1,29 +1,26 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 include_once "../corsheaders.php";
 include_once("../gapiv2/dbconn.php");
 include_once("../utils.php");
 
-$key = getParam('key', null);
-$id = getParam('product_id', null);
-$username = getParam('user_name', null);
-$useremail = getParam('user_email', null);
-$title = getParam('title', default: null);
-$content = getParam('content', null);
-$rating = getParam('rating', null);
+$id = getParam('product_id', "");
+$username = getParam('user_name', "");
+$useremail = getParam('user_email', "");
+$title = getParam('title',  "");
+$content = getParam('content', "");
+$rating = getParam('rating', "");
 
 $sql = "insert into reviews (product_id, user_name, user_email, rating, title, content) 
         values (?, ?, ?, ?, ?, ?)";
 
-echo $sql . "\n";
-var_dump([$id, $username, $useremail, $rating, $title, $content]);
-
-$row = executeSQL($sql, [$id, $username, $useremail, $rating, $title, $content]);
+$row = executeSQL($sql, [$id, $username, $useremail, $rating, $title, $content], []);
 if (!$row) {
     die(json_encode(['error' => 'Failed to create review.']));
 }
-
-
 
 $response = [
     'success' => true,
